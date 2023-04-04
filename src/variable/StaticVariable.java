5c63c34b024997630d15f6e04fb20911679d0f89
Vikash Basant <basantvikash360@gmail.com>
2023-04-04 11:00:21 +0530

    static varaiable concept practice some question

--- src/variable/StaticVariable.java
+++ src/variable/StaticVariable.java
@@ -1,7 +1,8 @@
 package variable;
 
 public class StaticVariable {
-    static int x = 10;
+    static int x; // static variable we don't need to initialization because JVM will automatically assign the
+    // default value by default.
     public static void main (String[] args) {
         System.out.println(StaticVariable.x);
         System.out.println(x);
