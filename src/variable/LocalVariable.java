78b554aa0d82ce598dfacb68aa316e99ba00701a
Vikash Basant <basantvikash360@gmail.com>
2023-04-04 11:44:33 +0530

    some practice question of the variable

---
+++ src/variable/LocalVariable.java
@@ -0,0 +1,7 @@
+package variable;
+
+public class LocalVariable {
+    public static void main (String[] args) {
+        final int x = 10;
+    }
+}
--- src/variable/StaticVariable.java
+++ src/variable/StaticVariable.java
@@ -1,12 +1,14 @@
 package variable;
 
 public class StaticVariable {
-    static int x; // static variable we don't need to initialization because JVM will automatically assign the
-    // default value by default.
-    public static void main (String[] args) {
-        System.out.println(StaticVariable.x);
-        System.out.println(x);
+    int x = 10;
+    static int y = 20;
+
+    public static void main(String[] args) {
         StaticVariable s = new StaticVariable();
-        System.out.println(s.x);
+        s.x = 30;
+        s.y = 40;
+        StaticVariable s2 = new StaticVariable();
+        System.out.println(s2.x + " " + s2.y);
     }
 }
